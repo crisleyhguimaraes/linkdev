90e50a59a49df82855ea0843b9d1b82607015596
crisleyhguimaraes <crisleyhguimaraes@gmail.com>
2022-07-08 20:31:37 -0300

    scroll2.0

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,6 @@
 window.sr = ScrollReveal({ reset:true});
 ScrollReveal({ distance: '15px' });
 sr.reveal('.home', {duration : 3000, origin: 'right'})
-sr.reveal('.logo', {duration : 3000, origin: 'left'})
-sr.reveal('.menu', {duration : 3000,  origin: 'left'})
-sr.reveal('.timing', {duration : 3000,  origin: 'left'})
 sr.reveal('.contact', { duration: 3000, origin: 'bottom' })
-sr.reveal('.icons', {duration : 3000, origin:'bottom'})
 sr.reveal('.services', { duration: 3000, origin: 'bottom' })
 sr.reveal('.footer', {duration : 3000, origin:'bottom'})
